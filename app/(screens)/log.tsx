24261bef0b804816e230b9c6b108641f4efded3b
Diazz <zaidsh02@gmail.com>
2025-12-12 21:12:39 +0100

    Using state to handle daily/weekly toggle

--- app/(screens)/log.tsx
+++ app/(screens)/log.tsx
@@ -1,7 +1,22 @@
 import {View, Text, TouchableOpacity} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+
+export interface CheckInData {
+    type: 'daily' | 'weekly';
+}
 
 const Log = () => {
+
+    const [checkInType, setCheckInType] = useState<'daily' | 'weekly'>('daily');
+
+    const [checkInData, setCheckInData] = useState<CheckInData>({
+        type: 'daily',
+    });
+
+    const updateStageData = (stageData: Partial<CheckInData>) => {
+        setCheckInData(prev => ({ ...prev, ...stageData }));
+    };
+
     return (
         <View className="flex-1 bg-background pt-12">
             <View className="mt-4 mb-5 px-4">
@@ -11,11 +26,21 @@ const Log = () => {
             </View>
             {/* Top toggle between daily and weekly */}
             <View className="flex-row justify-center gap-3">
-                <TouchableOpacity className="bg-background-dark rounded-xl p-5 items-center justify-center border border-[#D9D9D9] min-h-[5%] min-w-[45%]">
-                    <Text className="text-center text-secondary-dark text-[15px]">Daily</Text>
+                <TouchableOpacity
+                    onPress={() => {
+                        setCheckInType('daily');
+                        updateStageData({ type: 'daily' });
+                    }}
+                    className={`${checkInType === 'daily' ? 'bg-primary' : 'bg-background-dark'} rounded-xl p-5 items-center justify-center border border-[#D9D9D9] min-h-[5%] min-w-[45%]`}>
+                    <Text className={`${checkInType === 'daily' ? 'text-white': 'text-secondary-dark' }  text-center text-secondary-dark text-[15px]`}>Daily</Text>
                 </TouchableOpacity>
-                <TouchableOpacity className="bg-background-dark rounded-xl p-5 items-center justify-center border border-[#D9D9D9] min-h-[5%] min-w-[45%]">
-                    <Text className="text-center text-secondary-dark text-[15px]">Weekly</Text>
+                <TouchableOpacity
+                    onPress={() => {
+                        setCheckInType('weekly');
+                        updateStageData({ type: 'weekly' });
+                    }}
+                    className={`${checkInType === 'weekly' ? 'bg-primary' : 'bg-background-dark'} rounded-xl p-5 items-center justify-center border border-[#D9D9D9] min-h-[5%] min-w-[45%]`}>
+                    <Text className={`${checkInType === 'weekly' ? 'text-white': 'text-secondary-dark' } text-center  text-[15px]`}>Weekly</Text>
                 </TouchableOpacity>
             </View>
 
