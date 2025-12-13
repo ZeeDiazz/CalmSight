0c6c6d17998066c0cbc7c9b2befe5cdc46a5e387
Diazz <zaidsh02@gmail.com>
2025-12-13 14:49:23 +0100

    Moved Stage Nav at bottom. Daily/Weekly toggle only shown in stage 1.

--- app/(screens)/log.tsx
+++ app/(screens)/log.tsx
@@ -85,24 +85,28 @@ const Log = () => {
                 </Text>
             </View>
             {/* Top toggle between daily and weekly */}
-            <View className="flex-row justify-center gap-3">
-                <TouchableOpacity
-                    onPress={() => {
-                        setCheckInType('daily');
-                        updateStageData({ type: 'daily' });
-                    }}
-                    className={`${checkInType === 'daily' ? 'bg-primary' : 'bg-background-dark'} rounded-xl p-5 items-center justify-center border border-[#D9D9D9] min-h-[5%] min-w-[45%]`}>
-                    <Text className={`${checkInType === 'daily' ? 'text-white': 'text-secondary-dark' }  text-center text-secondary-dark text-[15px]`}>Daily</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => {
-                        setCheckInType('weekly');
-                        updateStageData({ type: 'weekly' });
-                    }}
-                    className={`${checkInType === 'weekly' ? 'bg-primary' : 'bg-background-dark'} rounded-xl p-5 items-center justify-center border border-[#D9D9D9] min-h-[5%] min-w-[45%]`}>
-                    <Text className={`${checkInType === 'weekly' ? 'text-white': 'text-secondary-dark' } text-center  text-[15px]`}>Weekly</Text>
-                </TouchableOpacity>
-            </View>
+            {currentStage ===1 ?
+                <View className="flex-row justify-center gap-3">
+                    <TouchableOpacity
+                        onPress={() => {
+                            setCheckInType('daily');
+                            updateStageData({ type: 'daily' });
+                        }}
+                        className={`${checkInType === 'daily' ? 'bg-primary' : 'bg-background-dark'} rounded-xl p-5 items-center justify-center border border-[#D9D9D9] min-h-[5%] min-w-[45%]`}>
+                        <Text className={`${checkInType === 'daily' ? 'text-white': 'text-secondary-dark' }  text-center text-secondary-dark text-[15px]`}>Daily</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => {
+                            setCheckInType('weekly');
+                            updateStageData({ type: 'weekly' });
+                        }}
+                        className={`${checkInType === 'weekly' ? 'bg-primary' : 'bg-background-dark'} rounded-xl p-5 items-center justify-center border border-[#D9D9D9] min-h-[5%] min-w-[45%]`}>
+                        <Text className={`${checkInType === 'weekly' ? 'text-white': 'text-secondary-dark' } text-center  text-[15px]`}>Weekly</Text>
+                    </TouchableOpacity>
+                </View>
+                :
+                <></>
+            }
 
             {/* Check-in stages*/}
             <View>
@@ -110,7 +114,7 @@ const Log = () => {
             </View>
 
             {/*Used to map the totalStages https://stackoverflow.com/questions/77705494/using-map-with-numbers-not-an-array-or-ignoring-the-first-parameter-in-a-map-a*/}
-            <View className="flex-row justify-center gap-3">
+            <View className="absolute bottom-[20%] flex-row left-0 right-0 justify-center gap-3">
                 {[...Array(totalStages)].map((_, index) => (
                     <View key={index} className={`w-3 h-3 rounded-full ${ index + 1 < currentStage ? 'bg-primary-dark'
                             : (index + 1 === currentStage
@@ -123,7 +127,7 @@ const Log = () => {
             </View>
 
             {/*Stage navigation buttons*/}
-            <View className="flex-row justify-center gap-3">
+            <View className="absolute bottom-[11%] left-0 right-0 flex-row justify-center gap-3">
                 <TouchableOpacity
                     onPress={handlePrevStage}
                     disabled={currentStage === 1}
